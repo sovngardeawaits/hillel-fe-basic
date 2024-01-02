23336e1278c8f426fdc177e50626bd244257f3fe
sovngardeawaits <smokingkills337@gmail.com>
2024-01-02 18:57:19 +0200

    finish hw15

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(`.burger`).click(function () {
+    $(this).toggleClass(`active`);
+    $(`.burger-nav`).toggleClass(`active`);
+
+});
+
+$('.close-menu').click(function() {
+    $('.burger-nav').removeClass('active');
+    $('.burger').removeClass('active');
+
+  });
+
+  $('.burger-nav').click(function() {
+    $('.burger-nav').removeClass('active');
+    $('.burger').removeClass('active');
+  });
+
